6beb509611b81bf52bdfe4116eb38950723d6c60
calynzkr <72541552+calynzkr@users.noreply.github.com>
2020-10-08 02:29:10 -0400

    Add files via upload

---
+++ tryy.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+
+def webscraper():
+    n=0
+    r = []
+    while n<50:
+        tmp = requests.get("http://3.95.249.159:8000/random_company").text    
+        r = r + [tmp]
+        n=n+1
+
+    sv=[]
+    H=[]
+    for i in r:
+        soup = BeautifulSoup(i, 'html.parser')
+        sv.append(soup.find('li').text)
+        ve = soup.get_text()
+        k = ve.splitlines()
+    
+        for j in k:
+            if j.startswith('Purpose'):
+                H.append(j)
+
+    fi = open("hw2results.txt", "a") 
+
+    for i in sv:
+        fi.write(i + "\n")
+    fi.close()  
+    fi = open("hw2results.txt", "a") 
+      
+    for g in H:
+        fi.write(g + "\n")
+    fi= open("hw2results.txt", "r")
+    fi.read()
+    fi.close()     
+
+if __name__ == "__main__": 
+    webscraper()
